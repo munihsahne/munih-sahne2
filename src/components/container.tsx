516ca092764f038862195c170f4c75eaba5eb010
munihsahne <munihsahne@gmail.com>
2026-05-24 14:28:31 +0300

    Replace old Next site with static Munih Sahne website

--- src/components/container.tsx
+++
@@ -1,4 +0,0 @@
-type Props = React.PropsWithChildren<{ className?: string }>;
-export default function Container({ children, className = "" }: Props) {
-  return <div className={`mx-auto w-full max-w-6xl px-5 ${className}`}>{children}</div>;
-}
